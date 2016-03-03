ce3e3e7eb24166c6aa1b87aa3264785589d04654
naoki-k <k12naoki@gmail.com>
2016-03-04 01:04:49 +0900

    Add new function that changes contents of slider by user

--- src/index.js
+++ src/index.js
@@ -5,4 +5,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+function getQueryParameter() {
+  let result = new Object
+  let query = document.location.search
+  if ( 1 < query.length ) {
+    let parameters = query.substring(1).split('&')
+    let element = parameters[0].split('=')
+    let paramKey = decodeURIComponent(element[0])
+    let paramValue = decodeURIComponent(element[1])
+    result[paramKey] = paramValue
+  }
+  return result
+}
+
+render(<App user={getQueryParameter()} ></App>, document.getElementById('root'));
